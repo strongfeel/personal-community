3a6420f7da704a25c3b96036f2c488064970e3ae
(GangPil_Song) <songgangpil@gmail.com>
2024-02-20 19:02:25 +0900

    프로젝트 셋팅

---
+++ src/main/java/personal/alcoholic/AlcoholicApplication.java
@@ -0,0 +1,13 @@
+package personal.alcoholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcoholicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/alcoholic/AlcoholicApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.alcoholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
